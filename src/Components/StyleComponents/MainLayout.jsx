84a1d57055cbc5e2e13c183d8a2e3f8c1f98b0bf
Fidelya1988 <oleksandram1988@gmail.com>
2021-07-18 23:25:33 +0300

    maked padding=0

--- src/Components/StyleComponents/MainLayout.jsx
+++ src/Components/StyleComponents/MainLayout.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="xl">
+      <Container maxWidth="xl" style={{ padding: "0" git }}>
         <Typography
-         component="div"
-         style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+          component="div"
+          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+        />
       </Container>
     </React.Fragment>
   );
