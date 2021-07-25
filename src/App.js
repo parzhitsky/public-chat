bde018179778083f8a950e81fbc1e7e28f87a14f
Fidelya1988 <oleksandram1988@gmail.com>
2021-07-25 21:23:29 +0300

    created basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MainLayout from './Components/StyleComponents/MainLayout'
+import MainLayout from './Components/MainLayout'
 function App() {
   return (
     <div className="App">
---
+++ src/Components/MainLayout.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import styles from './main.module.css'
+export default function SimpleContainer() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xl" style={{ padding: "0" }}>
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+        >
+          <div className={styles.container}>
+            <div className={styles.messages}>
+              <div className={styles.message}>Message</div>
+            </div>
+            <div className={styles.inputs}>
+              <textarea />
+              <div className={styles.typeName}>
+                Отправить как: <input />
+              </div>
+              <button>Отправить</button>
+            </div>
+          </div>
+        </Typography>
+      </Container>
+    </React.Fragment>
+  );
+}
